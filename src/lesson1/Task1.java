08f417c3ca62ed4a7d4b987704e2424ea9d8336b
IGOR ANF <ingvarr92@gmail.com>
2018-10-19 18:52:31 +0300

    First task

---
+++ src/lesson1/Task1.java
@@ -0,0 +1,19 @@
+package lesson1;
+
+public class Task1 {
+    public static void main (String [] args){
+        int le = 8;
+        int wi = 8;
+                if (le < wi) {
+                    System.out.println(wi);
+                }
+                else if (wi < le){
+                    System.out.println(le);
+                }
+                else {
+                    System.out.println("Это квадрат");
+                }
+
+    }
+
+}
