0573add73a8f0869621b3586197224eae16cbef3
IGOR ANF <ingvarr92@gmail.com>
2018-10-19 20:12:49 +0300

    Home work 1

---
+++ src/lesson1/Task3.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int oval1 = 15;
+        int oval2 = 600;
+        int oval3 = oval2/100;
+        int result = oval1 - oval3;
+        System.out.println(result);
+    }
+}
---
+++ src/lesson1/Task4.java
@@ -0,0 +1,7 @@
+package lesson1;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+    }
+}
