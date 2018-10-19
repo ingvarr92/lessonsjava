2e90e82759f307ea5625219c3188762238355013
IGOR ANF <ingvarr92@gmail.com>
2018-10-19 18:55:09 +0300

    First task

--- src/lesson1/Task1.java
+++ src/lesson1/Task1.java
@@ -5,10 +5,10 @@ public class Task1 {
         int le = 8;
         int wi = 8;
                 if (le < wi) {
-                    System.out.println(wi);
+                    System.out.println("Длинна больше");
                 }
                 else if (wi < le){
-                    System.out.println(le);
+                    System.out.println("Высота больше");
                 }
                 else {
                     System.out.println("Это квадрат");
